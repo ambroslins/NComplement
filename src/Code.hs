95545ba050e1e42387a04835bac6b6a6e7b35827
ambroslins <ambros.lins@gmail.com>
2020-09-02 11:33:10 +0200

    simplify code

--- src/Code.hs
+++ src/Code.hs
@@ -1,16 +1,35 @@
 module Code where
 
-import Code.G (G)
-import qualified Code.G as G
+import Axis (Axis)
+import qualified Axis
+import Control.Monad (forM)
+import Data.Foldable (toList)
+import Data.List.NonEmpty (NonEmpty)
+import qualified Data.Text as Text
+import Error
+import Expression (Expr)
+import qualified Expression as Expr
 import Generator
 import Parser
+import qualified Type
 
 data Code
-  = G G
+  = G00 (NonEmpty (Axis, Expr))
   deriving (Eq, Show)
 
 parser :: Parser Code
-parser = G <$> G.parser
+parser = symbol "G00" >> G00 <$> some p
+  where
+    p = do
+      a <- Axis.parser
+      e <- Expr.parser
+      pure (a, e)
 
 generate :: Code -> Gen ()
-generate (G g) = G.generate g
+generate (G00 as) = do
+  as' <- forM (toList as) $ \(a, expr) -> do
+    (t, e) <- Expr.eval expr
+    if t == Type.Real
+      then pure $ showText a <> e
+      else throwError Error
+  emit $ "G00 " <> Text.intercalate " " as'
