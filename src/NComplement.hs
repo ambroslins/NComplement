37c12bec53890da7bf9897f9f8a8a03e3b6bc4c8
ambroslins <ambros.lins@gmail.com>
2020-09-16 18:01:13 +0200

    add generate function

--- src/NComplement.hs
+++ src/NComplement.hs
@@ -8,6 +8,7 @@ import Gen
 import qualified Generator
 import qualified NC
 import qualified Parser
+import Syntax (Program)
 import System.FilePath
 import Text.Megaparsec (parse, pos1)
 
@@ -25,9 +26,12 @@ compile :: FilePath -> Text -> Either Error Text
 compile inFilePath input =
   case parse Parser.program (takeFileName inFilePath) input of
     Left e -> Left $ ParseError e
-    Right ast ->
-      NC.printStmts
-        <$> runGenerator env (Generator.program ast)
+    Right ast -> generate input ast
+
+generate :: Text -> Program -> Either Error Text
+generate input ast =
+  NC.printStmts
+    <$> runGenerator env (Generator.program ast)
   where
     env =
       Env
